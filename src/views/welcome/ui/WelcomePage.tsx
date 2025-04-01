fcf41e01d9adc1cf330abf52029abaa527b0dfc2
alexandr-obraztsov <alexlove2005@mail.ru>
2025-04-01 19:40:15 +0300

    fix: update welcome page design

--- src/views/welcome/ui/WelcomePage.tsx
+++ src/views/welcome/ui/WelcomePage.tsx
@@ -1,32 +1,33 @@
 'use client'
 
 import { PATH } from '@/shared/model'
-import { Button, Pagination } from '@/shared/ui'
-import Image from 'next/image'
+import { Button } from '@/shared/ui'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
-import box from 'public/images/box.png'
-import news from 'public/images/news.png'
-import phone from 'public/images/phone.png'
+import box from 'public/images/box.json'
+import newspaper from 'public/images/newspaper.json'
+import phone from 'public/images/phone.json'
 import React, { useState } from 'react'
+
+const Lottie = dynamic(() => import('lottie-react'), {
+	ssr: false,
+})
 
 const steps = [
 	{
-		img: phone,
+		lottie: phone,
 		title: 'Welcome to .news',
-		description:
-			'Organize your Telegram channels and reduce information noise with smart digests',
+		description: 'Organize Telegram channels and reduce noise',
 	},
 	{
-		img: box,
+		lottie: box,
 		title: 'Create Smart Digests',
-		description:
-			'Simply add your favorite Telegram channels to a digest and receive a concise summary of all important updates in one place',
+		description: 'Add channels to a digest and receive updates',
 	},
 	{
-		img: news,
+		lottie: newspaper,
 		title: 'Start Receiving News',
-		description:
-			'Get your personalized digest delivered right to your chat. Stay informed without the information overload',
+		description: 'Get personalized digest delivered to your chat',
 	},
 ]
 
@@ -61,37 +62,29 @@ export const WelcomePage = () => {
 
 	return (
 		<div
-			className='w-full h-screen p-[25px] '
+			className='w-full h-screen p-[25px] flex flex-col items-center justify-end'
 			onTouchStart={handleTouchStart}
 			onTouchMove={handleTouchMove}
 		>
-			<div className='relative w-full h-full flex flex-col items-center justify-start'>
-				<Image
-					src={step.img}
-					alt='image'
-					width={256}
-					height={256}
-					className='size-[256px]'
-				/>
-				<h1 className='text-primary font-bold text-xl mx-auto mt-[24px] select-none'>
-					{step.title}
-				</h1>
-				<p className='text-secondary text-balance mt-[28px] text-center text-base leading-[18px] h-[60px] select-none'>
-					{step.description}
-				</p>
-				<Pagination
-					sx='mt-[32px]'
-					pagesCount={steps.length}
-					currentPage={stepNum}
-				/>
-				<Button
-					sx='w-full py-[20px] absolute bottom-0'
-					variant='fulfilled'
-					onClick={handleNext}
-				>
-					Next
-				</Button>
+			<div id='progress' className='top-0 flex flex-row gap-1 w-full'>
+				<span className='block flex-1 h-1 rounded-full bg-accent'></span>
+				<span className='block flex-1 h-1 rounded-full bg-accent'></span>
+				<span className='block flex-1 h-1 rounded-full bg-stroke'></span>
 			</div>
+			<Lottie animationData={step.lottie} className='h-0 flex-1  my-4' />
+			<h1 className='shrink-0 mb-4 text-primary font-bold text-3xl mx-auto select-none'>
+				{step.title}
+			</h1>
+			<p className='font-sans font-medium shrink-0 mb-8 text-secondary text-center text-lg leading-[24px] select-none'>
+				{step.description}
+			</p>
+			<Button
+				sx='shrink-0 w-full py-[20px]'
+				variant='fulfilled'
+				onClick={handleNext}
+			>
+				Next
+			</Button>
 		</div>
 	)
 }
