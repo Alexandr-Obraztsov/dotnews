43f517f5074fae14a9ccef07f70154e5dde9b65f
alexandr-obraztsov <alexlove2005@mail.ru>
2025-04-04 13:35:53 +0300

    fix: update toast container settings

--- src/app/providers/ToastProvider.tsx
+++ src/app/providers/ToastProvider.tsx
@@ -7,15 +7,16 @@ export const ToastProvider = ({ children }: PropsWithChildren) => {
 	return (
 		<>
 			<ToastContainer
-				position='bottom-center'
-				autoClose={1000000}
+				position='top-center'
+				autoClose={5000}
 				hideProgressBar={false}
 				newestOnTop={false}
 				closeOnClick={false}
 				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
+				pauseOnFocusLoss={false}
+				draggable={true}
+				draggablePercent={40}
+				pauseOnHover={false}
 				transition={Bounce}
 				className='p-4 gap-2'
 				toastClassName='bg-foreground rounded-sm text-primary'
